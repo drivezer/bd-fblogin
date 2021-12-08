ea068bc0cdee449bda15e949c7bd29b518253c31
drivezer <dive-123@hotmail.co.th>
2021-12-09 02:33:19 +0700

    Merge: to main

---
+++ build/precache-manifest.6cb0703e5d568b0d91835fb2179a14dc.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b9bfd43f35f18337baa72d8808c52597",
+    "url": "/index.html"
+  },
+  {
+    "revision": "8d5b542713d354436276",
+    "url": "/static/css/main.7ef4da83.chunk.css"
+  },
+  {
+    "revision": "e11987582812957b0850",
+    "url": "/static/js/2.2ad3f1c7.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/static/js/2.2ad3f1c7.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "8d5b542713d354436276",
+    "url": "/static/js/main.514f6e22.chunk.js"
+  },
+  {
+    "revision": "e2702865a21868f2ebe6",
+    "url": "/static/js/runtime-main.7918a0be.js"
+  }
+]);
